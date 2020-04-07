9b46b85a55a9a72cf9cf11ebdcecd0500dba9390
shakib <hello@shakib.tech>
2020-04-08 02:24:28 +0600

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,7 +6,7 @@ const Menu = (props) => (
     <nav id="menu">
         <div className="inner">
             <ul className="links">
-                <li><Link onClick={props.onToggleMenu} to="/">হোম</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
                 <li><Link onClick={props.onToggleMenu} to="/landing">Landing</Link></li>
                 <li><Link onClick={props.onToggleMenu} to="/generic">Generic</Link></li>
                 <li><Link onClick={props.onToggleMenu} to="/elements">Elements</Link></li>
