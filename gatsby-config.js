c91046e340a92b220256b78962e8a278713bb792
shakib <hello@shakib.tech>
2020-04-08 02:34:32 +0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'করোনা ভাইরাস',
+        short_name: 'করোনা',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
