18abad44cf9cc6b30879f9c280ae9e8d622d80e4
shakib <hello@shakib.tech>
2020-04-08 17:48:24 +0600

    switched to bengali font

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,13 @@ module.exports = {
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
+      resolve: 'gatsby-plugin-google-fonts',
+      options: {
+        fonts: [
+          'Baloo Da 2', // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      },
     },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline'
